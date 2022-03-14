d71927b00b7805c09068365d5c7acae0263ea3b7
Your Name <you@example.com>
2022-03-14 04:43:53 -0700

    one

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,37 +6,49 @@
 
 /**
  *
- * * main - Evaluating random numbers
+ *  *main - entry point
  *
- * *
+ *   *
  *
- * * Return: Always 0 (Success)
+ *    *Description: tests whether a random number is negative or positive
  *
- * */
-
-
+ *     *
+ *
+ *      *Return: Always zero (successful)
+ *
+ *       */
 
 int main(void)
 
 {
 
+
 	int n;
+
 
 	srand(time(0));
 
 	n = rand() - RAND_MAX / 2;
 
+
 	if (n > 0)
+
+{
 
 		printf("%d is positive\n", n);
 
-	else if (n == 0)
+}
 
-		printf("%d is zero\n", n);
+	else if (n < 0)
 
-	else
+{
+												printf("%d is negative\n", n);
+										}
+											else if (n == 0)
 
-		printf("%d is negative\n", n);
+{	
 
-	return (0)
+		printf("%d is zero\n", n);							
+}	
+	return (0);
 }
