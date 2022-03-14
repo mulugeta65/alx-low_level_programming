28e40bf82ab54175e0c628f3ba94b7f13b19b9fc
Your Name <you@example.com>
2022-03-14 08:53:49 -0700

    advanced

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   * Return:0
+ *
+ *    */
+
+int main(void)
+
+{
+
+		int i, y;
+
+			for (i = 0; i < 10; i++)
+
+					{
+
+								putchar (i + '0');
+
+									}
+
+				for (y = 'a'; y <= 'f'; y++)
+
+						{
+
+									putchar (y);
+
+										}
+
+
+
+				putchar('\n');
+
+				return (0);
+
+}
+
+© 2022 GitHub, 
