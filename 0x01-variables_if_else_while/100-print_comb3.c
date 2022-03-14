c2a180733cb3d2fbdbfc501cbe7aef15422cfa50
mulugeta65 <95531260+mulugeta65@users.noreply.github.com>
2022-03-14 19:00:22 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,38 +1,47 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - prints a list of double didgits
- *
- * Description: digits should not be repeated twice
- * Return: Always(0) Success
-*/
-
+ * main - main block
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Numbers must be separated by ,, followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Print only the smallest combination of two digits
+ * Numbers should be printed in ascending order, with two digits
+ * You can only use the putchar function
+ * (every other function (printf, puts, etc…) is forbidden)
+ * You can only use putchar five times maximum in your code
+ * You are not allowed to use any variable of type char
+ * All your code should be in the main function
+ * Return: 0
+ */
 int main(void)
 {
-	int i, j;
+	int c;
+	int d = 0;
 
-	for (i = 48; i < 58; i++)
+	while (d < 10)
 	{
-		for (j = 48; j < 58; j++)
+		c = 0;
+		while (c < 10)
 		{
-			if (i == j)
+			if (d != c && d < c)
 			{
-				continue;
+				putchar('0' + d);
+				putchar('0' + c);
+
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-			putchar(i);
-			putchar(j);
-			if (i == 56 && j == 57)
-			{
-				break;
-			}
-			else
-			{
-				putchar(',');
-				putchar(' ');
-			}
+
+			c++;
 		}
+		d++;
 	}
 	putchar('\n');
-
 	return (0);
 }
